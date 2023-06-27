7bb3c337afcc5697f0a326e92761bfde2fa0c4da
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-27 10:32:09 +0530

    singup page frontend

---
+++ public/signup.js
@@ -0,0 +1,39 @@
+const myForm = document.getElementById('my-form');
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const numberInput  = document.getElementById('number');
+const passwordInput = document.getElementById('password');
+
+    myForm.addEventListener('submit', onSubmit);
+           
+    function onSubmit(e){
+        e.preventDefault();       
+        const name=nameInput.value;
+        const email=emailInput.value;
+        const number=numberInput.value;
+        const password=passwordInput.value;
+        const inputData={
+            name,
+            email,
+            number,
+            password,
+        };
+        console.log(inputData);
+        axios.post("http://localhost:3000/user/signup",inputData)
+            .then((response)=>{
+                console.log(response)
+                if(response.request.status==201){
+                alert(response.data.message);
+                window.location.href="./login.html";
+                }
+            })
+            .catch((err)=>{
+                console.log(err);
+                console.log(err.response.data.error);
+                document.body.innerHTML+=`<div style="color:red;">${err.response.data.error}<div>`; 
+            })
+            nameInput.value = '';
+            emailInput.value='';
+            passwordInput.value = '';  
+    }
