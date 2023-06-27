12300f096d4dcb2c9f06391a44762cbcd556619d
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-27 13:32:06 +0530

    backend files

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+
+const cors = require('cors');
+
+
+const sequelize = require('./util/database');
+
+const app = express();
+
+app.use(cors());
+
+const userRoutes = require('./routes/user');
+
+app.use(express.json());
+
+app.use('/user',userRoutes);
+
+sequelize.sync()
+.then(()=>app.listen(process.env.HOST||3000))
+.catch((err)=>console.log(err));
+
---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+const signupController = require('../controllers/signup');
+
+router.post('/signup',signupController.postSignUp);
+
+module.exports = router;
