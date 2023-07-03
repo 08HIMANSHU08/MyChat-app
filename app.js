106c6c278f1d7f0efdd2f0f404b4d1f0206d5126
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-03 15:01:47 +0530

    main server file after using cron

--- app.js
+++ app.js
@@ -1,30 +1,82 @@
+
 const express = require('express');
-require('dotenv').config();
-
+const path = require('path');
 const cors = require('cors');
-
-
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
 const sequelize = require('./util/database');
+var CronJob = require('cron').CronJob;
 
-const userModel = require('./models/user');
-const messageModel = require('./models/messages');
+const User = require('./models/user');
+const Message = require('./models/message');
+const Group = require('./models/group');
+const GroupUser = require('./models/groupUser');
+const Forgotpassword = require('./models/forgot-password');
+const Files = require('./models/groupfiles');
+const Archieve = require('./models/archieve-chat');
 
 const app = express();
 
 app.use(cors({
-    origin:'*',
+    origin : '*',
 }));
 
+app.use(bodyParser.json());
+
+
+User.hasMany(Message);
+Message.belongsTo(User);
+
+Group.belongsToMany(User, {through: GroupUser});
+User.belongsToMany(Group, {through: GroupUser});
+
+Group.hasMany(Message);
+Message.belongsTo(Group);
+
+Group.hasMany(Files);
+
+User.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(User);
+
 const userRoutes = require('./routes/user');
+const messageRoutes = require('./routes/message');
+const chatRoutes = require('./routes/chat');
+const adminRoutes = require('./routes/admin');
+const passwordRoutes = require('./routes/password');
+const fileRoutes = require('./routes/group-files');
 
-app.use(express.json());
+app.use('/user', userRoutes);
+app.use('/message', messageRoutes);
+app.use('/chat', chatRoutes);
+app.use('/admin', adminRoutes);
+app.use('/password', passwordRoutes);
+app.use('/file', upload.single('myfile'),fileRoutes);
 
-app.use('/user',userRoutes);
-
-userModel.hasMany(messageModel);
-messageModel.belongsTo(userModel);
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, `${req.url}`));
+});
 
 sequelize.sync()
-.then(()=>app.listen(process.env.HOST||3000))
-.catch((err)=>console.log(err));
+    .then(() => {
+       app.listen(process.env.PORT)
+        console.log('port listining on 3000');
 
+        new CronJob('0 0 * * *', async function() {
+            const chats = await Message.findAll();
+            console.log('chats *********',chats);
+        
+            for (const chat of chats) {
+                await Archieve.create({ groupId: chat.groupId, userId: chat.userId, message: chat.message });
+                console.log('id hai yaar',chat.id)
+                await Message.destroy({where:{id:chat.id}})
+            }
+            },
+            null,
+            true,
+        );
+    })
+    .catch(error => console.log("error in appjs file"));
+
+   
